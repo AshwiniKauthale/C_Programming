530c5932caab0be8566ac9099c2f414b6949fcba
AshwiniKauthale <kauthaleashwini40@gmail.com>
2025-11-23 13:29:03 +0530

    Add files via upload

---
+++ Assigmnents/Assignment_29/program29_2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void Display(int iRow, int iCol)
+{
+    int i = 0, j = 0,iCnt = 1;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        if ((i % 2) == 0)
+        {
+            iCnt = 1;
+        }
+        else
+        {
+            iCnt = 2;
+        }
+        for(j = 0; j <= iCol; j++)
+        {
+            printf("%d\t",iCnt);
+            iCnt = iCnt + 2;
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of rows :");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of columns :");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
